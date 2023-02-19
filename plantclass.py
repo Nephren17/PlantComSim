e927feb7c76ab1ce25eb6d08023b1852c2dfb584
Nephren17 <nephren17@outlook.com>
2023-02-19 16:18:42 +0800

    add some values setting

--- plantclass.py
+++ plantclass.py
@@ -16,6 +16,8 @@ class Plant:
         self.color=""
         pass
 
+# Plant suitable for Tropical savanna climate and tropical desert climate
+
 class Cactus(Plant):   #仙人掌
     def __init__(self, x, y, r=3) -> None:
         super().__init__(x, y, r)
@@ -27,27 +29,11 @@ class Cactus(Plant):   #仙人掌
         self.l_min=2000
         self.l_max=8000
         self.life=1000
-        self.water=1000
-        self.repro_period=3 #Years are set as the unit for reproduction period
-        self.repro_eff=20 #The value represent how many seed are likely to grow up in one cycle
+        self.water=1500
+        self.repro_period=40
+        self.water_consume=30
+        self.soil_consume=30
         self.color="#6abe83"
-
-class Arteannuin(Plant):  #黄花蒿 
-    def __init__(self, x, y, r=3) -> None:
-        super().__init__(x, y, r)
-        self.name="Arteannuin"
-        self.t_min=-20
-        self.t_max=35
-        self.h_min=0.3
-        self.h_max=0.8
-        self.l_min=500
-        self.l_max=2000
-        self.life=100
-        self.water=100
-        self.repro_period=0.3333
-        self.repro_eff=30
-        self.color="#6abe83"
-        self.color="#F0D879"
 
 class Hippophae(Plant):  #沙棘 
     def __init__(self, x, y, r=3) -> None:
@@ -59,11 +45,87 @@ class Hippophae(Plant):  #沙棘
         self.h_max=0.8
         self.l_min=1000
         self.l_max=2000
-        self.life=700
-        self.water=500
-        self.repro_period=3
-        self.repro_eff=10
+        self.life=150
+        self.water=100
+        self.repro_period=20
+        self.water_consume=3
+        self.soil_consume=3
         self.color="#F78D3F"
+
+class Thorn(Plant):  #荆条
+    def __init__(self, x, y, r=3) -> None:
+        super().__init__(x, y, r)
+        self.name="Thorn"
+        self.t_min=-20
+        self.t_max=30
+        self.h_min=0.4
+        self.h_max=0.8
+        self.l_min=2000
+        self.l_max=60000
+        self.life=150
+        self.water=100
+        self.repro_period=20
+        self.water_consume=3
+        self.soil_consume=3
+        self.color="#393939"
+
+
+
+# Plant suitable for Tropical savanna and temperate grassland
+class Stipa(Plant):   #针茅
+    def __init__(self, x, y, r=3) -> None:
+        super().__init__(x, y, r)
+        self.name="Stipa"
+        self.t_min=-25
+        self.t_max=40
+        self.h_min=0.1
+        self.h_max=0.8
+        self.l_min=1500
+        self.l_max=5000
+        self.life=100
+        self.water=120
+        self.repro_period=2.5
+        self.water_consume=5
+        self.soil_consume=5
+        self.color="#FF5A09"
+
+class Villous_themeda(Plant):   #菅子草
+    def __init__(self, x, y, r=3) -> None:
+        super().__init__(x, y, r)
+        self.name="Villous_themeda"
+        self.t_min=-6
+        self.t_max=32
+        self.h_min=0.3
+        self.h_max=0.7
+        self.l_min=1000
+        self.l_max=4000
+        self.life=100
+        self.water=120
+        self.repro_period=4
+        self.water_consume=5
+        self.soil_consume=5
+        self.color="#00303F"
+
+
+# Plant suitable for Temperate grassland
+class Arteannuin(Plant):  #黄花蒿 
+    def __init__(self, x, y, r=3) -> None:
+        super().__init__(x, y, r)
+        self.name="Arteannuin"
+        self.t_min=-20
+        self.t_max=35
+        self.h_min=0.3
+        self.h_max=0.8
+        self.l_min=500
+        self.l_max=2000
+        self.life=200
+        self.water=150
+        self.repro_period=3
+        self.water_consume=6
+        self.soil_consume=6
+        self.color="#7A9D96"
+
+
 
 class Poplar(Plant):  #胡杨
     def __init__(self, x, y, r=3) -> None:
@@ -75,11 +137,119 @@ class Poplar(Plant):  #胡杨
         self.h_max=0.7
         self.l_min=500
         self.l_max=2000
-        self.life=20000
-        self.water=20000
-        self.repro_period=8
-        self.repro_eff=1
-        self.color="#248888"
+        self.life=16000
+        self.water=9000
+        self.repro_period=10000000
+        self.water_consume=150
+        self.soil_consume=120
+        self.color="#F6C325"
+
+class Argy(Plant):  #艾草
+    def __init__(self, x, y, r=3) -> None:
+        super().__init__(x, y, r)
+        self.name="Argy"
+        self.t_min=-15
+        self.t_max=35
+        self.h_min=0.4
+        self.h_max=0.7
+        self.l_min=400
+        self.l_max=2500
+        self.life=100
+        self.water=100
+        self.repro_period=4
+        self.water_consume=5
+        self.soil_consume=5
+        self.color="#FA8072"
+
+
+class Crofton_weed(Plant):  #紫茎泽兰
+    def __init__(self, x, y, r=3) -> None:
+        super().__init__(x, y, r)
+        self.name="Crofton_weed"
+        self.t_min=-5
+        self.t_max=30
+        self.h_min=0.3
+        self.h_max=0.8
+        self.l_min=1000
+        self.l_max=5000
+        self.life=140
+        self.water=120
+        self.repro_period= 1.2
+        self.water_consume=8
+        self.soil_consume=8
+        self.color="#7FFFD4"
+
+class Leymus_chinensis(Plant):  #羊草
+    def __init__(self, x, y, r=3) -> None:
+        super().__init__(x, y, r)
+        self.name="Leymus_chinensis"
+        self.t_min=-20
+        self.t_max=35
+        self.h_min=0.3
+        self.h_max=0.8
+        self.l_min=500
+        self.l_max=2000
+        self.life=100
+        self.water=100
+        self.repro_period=4
+        self.water_consume=5
+        self.soil_consume=5
+        self.color="#800080"
+
+
+class Locust(Plant):  #刺槐
+    def __init__(self, x, y, r=3) -> None:
+        super().__init__(x, y, r)
+        self.name="Locust"
+        self.t_min=-25
+        self.t_max=35
+        self.h_min=0.3
+        self.h_max=0.8
+        self.l_min=500
+        self.l_max=3000
+        self.life=14000
+        self.water=15000
+        self.repro_period=6
+        self.water_consume=150
+        self.soil_consume=150
+        self.color="#FF00FF"
+
+# Plant that is suitable for all the three  types of climate
+
+class Chinese_pennisetum(Plant):  #狼尾草
+    def __init__(self, x, y, r=3) -> None:
+        super().__init__(x, y, r)
+        self.name="Chinese_pennisetum"
+        self.t_min=-15
+        self.t_max=35
+        self.h_min=0.3
+        self.h_max=0.7
+        self.l_min=3000
+        self.l_max=60000
+        self.life=100
+        self.water=100
+        self.repro_period=6
+        self.water_consume=5
+        self.soil_consume=5
+        self.color="#8F00FF"
+
+class Conyza_canadensis(Plant):  #飞蓬
+    def __init__(self, x, y, r=3) -> None:
+        super().__init__(x, y, r)
+        self.name="Conyza_canadensis"
+        self.t_min=-10
+        self.t_max=35
+        self.h_min=0.2
+        self.h_max=0.7
+        self.l_min=500
+        self.l_max=2000
+        self.life=100
+        self.water=100
+        self.repro_period=6
+        self.water_consume=6
+        self.soil_consume=6
+        self.color="#00FFFF"
+
 
 cac = Cactus(2,3)
 print(cac.l_min)
